c68245686da05aaf34a46c78c4dfd7d8e8ccb77c
Sam Morris <45403038+sammorrisdesign@users.noreply.github.com>
2021-11-10 16:53:19 -0500

    Get suggestions instead of old copy from Google Docs

--- src/docs/get.js
+++ src/docs/get.js
@@ -2,6 +2,7 @@ export default async function get(docId) {
   const { data } = await this.docsAPI.documents.get({
     auth: this.client,
     documentId: docId,
+    suggestionsViewMode: 'PREVIEW_SUGGESTIONS_ACCEPTED'
   });
 
   return data;
